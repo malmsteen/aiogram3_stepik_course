e700a5db99d8c45c0ff25bd4a2445f17f6f76919
kmsint <kmsrus@gmail.com>
2025-07-08 16:57:38 +0300

    refactor: improove callback buttons examples

--- inline_buttons/callback_buttons_1.py
+++ inline_buttons/callback_buttons_1.py
@@ -1,30 +1,28 @@
+import os
+
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.types import (CallbackQuery, InlineKeyboardButton,
-                           InlineKeyboardMarkup, Message)
-
-# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
-# полученный у @BotFather
-BOT_TOKEN = 'BOT TOKEN HERE'
+from aiogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 
 # Создаем объекты бота и диспетчера
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher()
 
 # Создаем объекты инлайн-кнопок
-big_button_1 = InlineKeyboardButton(
-    text='БОЛЬШАЯ КНОПКА 1',
-    callback_data='big_button_1_pressed'
+button_1 = InlineKeyboardButton(
+    text="КНОПКА 1", callback_data="button_1_click"
 )
-big_button_2 = InlineKeyboardButton(
-    text='БОЛЬШАЯ КНОПКА 2',
-    callback_data='big_button_2_pressed'
+button_2 = InlineKeyboardButton(
+    text="КНОПКА 2", callback_data="button_2_click"
 )
 
 # Создаем объект инлайн-клавиатуры
-keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[big_button_1],
-                     [big_button_2]])
+keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2]])
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
@@ -32,18 +30,36 @@ keyboard = InlineKeyboardMarkup(
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(
-        text='Это инлайн-кнопки. Нажми на любую!',
-        reply_markup=keyboard
+        text="Это инлайн-кнопки. Нажми на любую!", reply_markup=keyboard
+    )
+
+
+# # Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
+# # с data `button_1_click` или `button_2_click`
+# @dp.callback_query(F.data.in_(["button_1_click", "button_2_click"]))
+# async def process_buttons_click(callback: CallbackQuery):
+#     await callback.answer()
+
+
+# Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
+# с data `button_1_click`
+@dp.callback_query(F.data == 'button_1_click')
+async def process_button_1_click(callback: CallbackQuery):
+    await callback.message.edit_text(
+        text='Была нажата КНОПКА 1',
+        reply_markup=callback.message.reply_markup
     )
 
 
 # Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
-# с data 'big_button_1_pressed' или 'big_button_2_pressed'
-@dp.callback_query(F.data.in_(['big_button_1_pressed',
-                               'big_button_2_pressed']))
-async def process_buttons_press(callback: CallbackQuery):
-    await callback.answer()
+# с data `button_2_click`
+@dp.callback_query(F.data == 'button_2_click')
+async def process_button_2_click(callback: CallbackQuery):
+    await callback.message.edit_text(
+        text='Была нажата КНОПКА 2',
+        reply_markup=callback.message.reply_markup
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dp.run_polling(bot)
--- inline_buttons/callback_buttons_2.py
+++ inline_buttons/callback_buttons_2.py
@@ -1,30 +1,28 @@
+import os
+
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.types import (CallbackQuery, InlineKeyboardButton,
-                           InlineKeyboardMarkup, Message)
-
-# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
-# полученный у @BotFather
-BOT_TOKEN = 'BOT TOKEN HERE'
+from aiogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 
 # Создаем объекты бота и диспетчера
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher()
 
 # Создаем объекты инлайн-кнопок
-big_button_1 = InlineKeyboardButton(
-    text='БОЛЬШАЯ КНОПКА 1',
-    callback_data='big_button_1_pressed'
+button_1 = InlineKeyboardButton(
+    text="КНОПКА 1", callback_data="button_1_click"
 )
-big_button_2 = InlineKeyboardButton(
-    text='БОЛЬШАЯ КНОПКА 2',
-    callback_data='big_button_2_pressed'
+button_2 = InlineKeyboardButton(
+    text="КНОПКА 2", callback_data="button_2_click"
 )
 
 # Создаем объект инлайн-клавиатуры
-keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[big_button_1],
-                     [big_button_2]])
+keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2]])
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
@@ -32,34 +30,33 @@ keyboard = InlineKeyboardMarkup(
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(
-        text='Это инлайн-кнопки. Нажми на любую!',
-        reply_markup=keyboard
+        text="Это инлайн-кнопки. Нажми на любую!", reply_markup=keyboard
     )
 
 
 # Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
-# с data 'big_button_1_pressed'
-@dp.callback_query(F.data == 'big_button_1_pressed')
-async def process_button_1_press(callback: CallbackQuery):
-    if callback.message.text != 'Была нажата БОЛЬШАЯ КНОПКА 1':
+# с data `button_1_click`
+@dp.callback_query(F.data == "button_1_click")
+async def process_button_1_click(callback: CallbackQuery):
+    if callback.message.text != "Была нажата КНОПКА 1":
         await callback.message.edit_text(
-            text='Была нажата БОЛЬШАЯ КНОПКА 1',
-            reply_markup=callback.message.reply_markup
+            text="Была нажата КНОПКА 1",
+            reply_markup=callback.message.reply_markup,
         )
     await callback.answer()
 
 
 # Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
-# с data 'big_button_2_pressed'
-@dp.callback_query(F.data == 'big_button_2_pressed')
-async def process_button_2_press(callback: CallbackQuery):
-    if callback.message.text != 'Была нажата БОЛЬШАЯ КНОПКА 2':
+# с data `button_2_click`
+@dp.callback_query(F.data == "button_2_click")
+async def process_button_2_click(callback: CallbackQuery):
+    if callback.message.text != "Была нажата КНОПКА 2":
         await callback.message.edit_text(
-            text='Была нажата БОЛЬШАЯ КНОПКА 2',
-            reply_markup=callback.message.reply_markup
+            text="Была нажата КНОПКА 2",
+            reply_markup=callback.message.reply_markup,
         )
     await callback.answer()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dp.run_polling(bot)
